add3d928990c4e87736343a63e2492f0df5e2133
Silent YANG <silenty4ng@gmail.com>
2022-06-02 01:39:30 +0800

    first commit

---
+++ src/main/java/com/ctfdj/ctfdj/CtfdjApplication.java
@@ -0,0 +1,13 @@
+package com.ctfdj.ctfdj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtfdjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtfdjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctfdj/ctfdj/CtfdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctfdj.ctfdj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtfdjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
